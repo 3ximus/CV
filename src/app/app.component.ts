bdd96492c944cf9be4d973bf4343b3289e11a80a
eximus <fabio4335@gmail.com>
2021-08-13 13:39:19 +0100

    add highlight when scrolling the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    private NUMBER_OF_PAGES:number = 4;
+    page : number = 1; // this is used to control index highlight
 
     scrollTo(el : HTMLElement) {
         const y: number = el.getBoundingClientRect().top + window.pageYOffset;
         window.scrollTo({top: y, behavior: 'smooth'});
     }
+
+    @HostListener("window:scroll", ["$event"])
+    onWindowScroll(event:Event){
+        this.page = Math.floor((window.scrollY + 100) / (this.getDocumentHeight() / this.NUMBER_OF_PAGES) + 1);
+        console.log(this.page);
+    }
+
+    // helper function to get the correct document height
+    getDocumentHeight() : number {
+        return Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+        );
+    }
+
 }
