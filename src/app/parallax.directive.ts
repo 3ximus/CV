12e66be8c5a7d4656d0073e74359040c3eb240fe
eximus <fabio4335@gmail.com>
2021-08-13 13:00:26 +0100

    remove parallax effect

--- src/app/parallax.directive.ts
+++ src/app/parallax.directive.ts
@@ -1,20 +1,20 @@
 import { Directive, Input, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appParallax]'
+    selector: '[parallaxEffect]'
 })
 export class ParallaxDirective {
 
-  @Input('ratio') parallaxRatio : number = 1
-  initialTop : number = 0
+    @Input('ratio') parallaxRatio : number = 1
+    initialTop : number = 0
 
-  constructor(private eleRef : ElementRef) {
-    this.initialTop = this.eleRef.nativeElement.getBoundingClientRect().top
-  }
+    constructor(private eleRef : ElementRef) {
+        this.initialTop = this.eleRef.nativeElement.getBoundingClientRect().top
+    }
 
-  @HostListener("window:scroll", ["$event"])
-  onWindowScroll(event:Event){
-    this.eleRef.nativeElement.style.top = (this.initialTop - (window.scrollY * this.parallaxRatio)) + 'px'
-  }
+    @HostListener("window:scroll", ["$event"])
+    onWindowScroll(event:Event){
+        this.eleRef.nativeElement.style.top = (this.initialTop - (window.scrollY * this.parallaxRatio)) + 'px'
+    }
 
 }
