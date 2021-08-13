b6e232d8f0ff55ab63d8bf2ee62b8ba521d98a02
eximus <fabio4335@gmail.com>
2021-08-13 20:50:14 +0100

    compact skills and add skill level

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+	selector: 'app-skills',
+	templateUrl: './skills.component.html',
+	styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+	skills: {name:string, value:number}[] = [
+		{ name: "Angular JS"     , value: 50 },
+		{ name: "Python"         , value: 90 },
+		{ name: "Django"         , value: 80 },
+		{ name: "Flask"          , value: 60 },
+		{ name: "MongoDB"        , value: 60 },
+		{ name: "Docker"         , value: 60 },
+		{ name: "SQL"            , value: 50 },
+		{ name: "Git"            , value: 90 },
+		{ name: "Sass"           , value: 60 },
+		{ name: "Bash scripting" , value: 90 },
+		{ name: "Java"           , value: 70 },
+		{ name: "C/C++"          , value: 70 },
+		{ name: "LaTeX"          , value: 50 },
+	];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
