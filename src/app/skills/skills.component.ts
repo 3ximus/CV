15bc413c53020df90d69da5b0f0d09df14854ecc
eximus <fabio4335@gmail.com>
2021-08-16 16:55:43 +0100

    add icon backdrop for each skill on hover

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import { IconDefinition, faGit, faAngular, faPython, faDocker, faSass, faJava } from '@fortawesome/free-brands-svg-icons';
+import {faDatabase, faCopyright, faTerminal, faFileAlt} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
 	selector: 'app-skills',
@@ -6,25 +10,35 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
-	skills: {name:string, value:number}[] = [
-		{ name: "Angular JS"     , value: 50 },
-		{ name: "Python"         , value: 90 },
-		{ name: "Django"         , value: 80 },
-		{ name: "Flask"          , value: 60 },
-		{ name: "MongoDB"        , value: 60 },
-		{ name: "Docker"         , value: 60 },
-		{ name: "SQL"            , value: 50 },
-		{ name: "Git"            , value: 90 },
-		{ name: "Sass"           , value: 60 },
-		{ name: "Bash scripting" , value: 90 },
-		{ name: "Java"           , value: 70 },
-		{ name: "C/C++"          , value: 70 },
-		{ name: "LaTeX"          , value: 50 },
+	skills: {name:string, value:number, icon:any}[] = [
+		{ name: "Angular JS"	 , value: 50 , icon: faAngular},
+		{ name: "Python"		 , value: 90 , icon: faPython},
+		{ name: "Django"		 , value: 80 , icon: faPython},
+		{ name: "Flask"			 , value: 60 , icon: faPython},
+		{ name: "MongoDB"		 , value: 60 , icon: faDatabase},
+		{ name: "Docker"		 , value: 60 , icon: faDocker},
+		{ name: "SQL"			 , value: 50 , icon: faDatabase},
+		{ name: "Git"			 , value: 90 , icon: faGit},
+		{ name: "Sass"			 , value: 60 , icon: faSass},
+		{ name: "Bash scripting" , value: 90 , icon: faTerminal},
+		{ name: "Java"			 , value: 70 , icon: faJava},
+		{ name: "C/C++"			 , value: 70 , icon: faCopyright},
+		{ name: "LaTeX"			 , value: 50 , icon: faFileAlt},
 	];
+	backdropIcon:IconDefinition = faGit;
+	@ViewChild('backdropElement') backdropElement! : ElementRef;
 
 	constructor() { }
 
 	ngOnInit(): void {
 	}
 
+	changeIcon(ico:IconDefinition) : void {
+		this.backdropIcon = ico;
+		this.backdropElement.nativeElement.style.opacity = 0.04;
+	}
+
+	hideIcon(): void {
+		this.backdropElement.nativeElement.style.opacity = 0;
+	}
 }
