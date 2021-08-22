3f6d30d3a7bbda345ab1115a758a3edfa931c7c8
eximus <fabio4335@gmail.com>
2021-08-22 12:03:11 +0100

    improve skill component background

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { IconDefinition, faGit, faAngular, faPython, faDocker, faSass, faJava, faJsSquare } from '@fortawesome/free-brands-svg-icons';
+import { IconDefinition, faGitAlt, faAngular, faPython, faDocker, faSass, faJava, faJsSquare } from '@fortawesome/free-brands-svg-icons';
 import {faDatabase, faCopyright, faTerminal, faFileAlt, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -10,7 +10,7 @@ import {faDatabase, faCopyright, faTerminal, faFileAlt, faMobileAlt } from '@for
 	styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
-	skills: {name:string, value:number, icon:any}[] = [
+	skills: {name:string, value:number, icon:IconDefinition}[] = [
 		{ name: "Angular 2"	     , value: 65 , icon: faAngular},
 		{ name: "JavaScript"	 , value: 60 , icon: faJsSquare},
 		{ name: "Python"		 , value: 90 , icon: faPython},
@@ -18,27 +18,16 @@ export class SkillsComponent implements OnInit {
 		{ name: "Flutter"		 , value: 35 , icon: faMobileAlt},
 		{ name: "Docker"		 , value: 60 , icon: faDocker},
 		{ name: "SQL"			 , value: 50 , icon: faDatabase},
-		{ name: "Git"			 , value: 90 , icon: faGit},
+		{ name: "Git"			 , value: 90 , icon: faGitAlt},
 		{ name: "Sass"			 , value: 60 , icon: faSass},
 		{ name: "Bash scripting" , value: 90 , icon: faTerminal},
 		{ name: "Java"			 , value: 70 , icon: faJava},
 		{ name: "C/C++"			 , value: 70 , icon: faCopyright},
 		{ name: "LaTeX"			 , value: 50 , icon: faFileAlt},
 	];
-	backdropIcon:IconDefinition = faGit;
-	@ViewChild('backdropElement') backdropElement! : ElementRef;
 
 	constructor() { }
 
 	ngOnInit(): void {
-	}
-
-	changeIcon(ico:IconDefinition) : void {
-		this.backdropIcon = ico;
-		this.backdropElement.nativeElement.style.opacity = 0.04;
-	}
-
-	hideIcon(): void {
-		this.backdropElement.nativeElement.style.opacity = 0;
 	}
 }
